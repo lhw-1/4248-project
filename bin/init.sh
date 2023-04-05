329e288bd2b9a1b0a13b1144bdc9a30fcb4715ae
ktyap <kimthow.yap@gmail.com>
2023-04-05 17:14:05 +0800

    download links cleanups

--- bin/init.sh
+++ bin/init.sh
@@ -30,10 +30,10 @@ cd ..
 #curl -o test.csv https://raw.githubusercontent.com/OanaMariaCamburu/e-SNLI/master/dataset/esnli_test.csv
 
 mkdir dataset
-curl -o dataset/esnli_train_1.csv https://github.com/OanaMariaCamburu/e-SNLI/blob/master/dataset/esnli_train_1.csv
-curl -o dataset/esnli_train_2.csv https://github.com/OanaMariaCamburu/e-SNLI/blob/master/dataset/esnli_train_2.csv
-curl -o dataset/esnli_dev.csv https://github.com/OanaMariaCamburu/e-SNLI/blob/master/dataset/esnli_dev.csv
-curl -o dataset/esnli_test.csv https://github.com/OanaMariaCamburu/e-SNLI/blob/master/dataset/esnli_test.csv
+curl -o dataset/esnli_train_1.csv https://raw.githubusercontent.com/OanaMariaCamburu/e-SNLI/master/dataset/esnli_train_1.csv
+curl -o dataset/esnli_train_2.csv https://raw.githubusercontent.com/OanaMariaCamburu/e-SNLI/master/dataset/esnli_train_2.csv
+curl -o dataset/esnli_dev.csv https://raw.githubusercontent.com/OanaMariaCamburu/e-SNLI/master/dataset/esnli_dev.csv
+curl -o dataset/esnli_test.csv https://raw.githubusercontent.com/OanaMariaCamburu/e-SNLI/master/dataset/esnli_test.csv
 mkdir dataset/GloVe
 curl -o dataset/GloVe/glove.840B.300d.zip http://nlp.stanford.edu/data/glove.840B.300d.zip
 unzip dataset/GloVe/glove.840B.300d.zip -d dataset/GloVe/
@@ -42,11 +42,16 @@ rm -r dataset/GloVe/glove.840B.300d.zip
 #download infersent encoders
 #infersent1.pkl
 gdown 1csv3pP-tikFZHWLEVLDMqSVDnPx77AQz
-#intersent2.pkl
+#intersent2.pkl (not used)
 gdown 1NaB79RJEW79VLHo0WBCR1SxLwbsTUUd5
 
-#download trained InferSent model
-gdown 1peT6jGH63zzYroqLpWo_eGZBE5E3PnGM
+#download trained InferSent NLI model
+gdown 19HBWkX-FY6inoAjseRJ4zhp2GrLM4DjB
+
+#download trained SentenceTransformer encoder
+gdown 1geWoPK33bpS8K9kPyMQxdbvU8i6lNmjH
+#download train SentenceTransformer NLI model
+gdown 1wnKo6wgmQlW5rV5pvIajQsEoDOAQIHf7
 
 # Cleanup
 cd ..
